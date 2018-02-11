75f813619f6dc702d1ec080b2a8963f6545797c7
shellyln <shellyln@users.noreply.github.com>
2018-02-11 22:52:27 +0900

    Move python lambda call interface to red-agate. Add CLI params to main app.

--- app_main.py
+++ app_main.py
@@ -1,34 +1,11 @@
 #!/usr/bin/env python
 
-import subprocess
-import sys
-import os
 import json
+import os
+import sys
 
-
-class AppInternalServerErrorException(Exception):
-    pass
-
-
-def call_node_lambda(command=None, event=None, timeout=None):
-    event_ = event
-    if not isinstance(event_, str):
-        event_ = json.dumps(event_, separators=(',', ':'))
-    with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE) as proc:
-        proc.stdin.write(event_.encode('UTF-8'))
-        try:
-            proc_status = -1
-            (output, err) = proc.communicate(timeout=timeout)
-            proc_status = proc.wait(timeout=0)
-        except subprocess.TimeoutExpired:
-            proc.kill()
-            (output, err) = proc.communicate()
-        if proc_status != 0:
-            raise AppInternalServerErrorException()
-        elif err is not None:
-            raise AppInternalServerErrorException()
-        return output
-
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../red-agate/')
+from redagate_lambda import call, AppInternalServerErrorException
 
 
 if __name__ == '__main__':
@@ -44,21 +21,21 @@ if __name__ == '__main__':
         with open('./src/examples/barcode.data.json') as f:
             event = json.loads(f.read())
             event['eventName'] = '/'
-            return call_node_lambda(command=["node", "dist/app.js"], event=event)
+            return call(command=["node", "dist/app.js"], event=event)
 
     @app.route('/barcode')
     def run_barcode_test_report():
         with open('./src/examples/barcode.data.json') as f:
             event = json.loads(f.read())
             event['eventName'] = '/barcode-test'
-            return call_node_lambda(command=["node", "dist/app.js"], event=event)
+            return call(command=["node", "dist/app.js"], event=event)
 
     @app.route('/billing')
     def run_billing_report():
         with open('./src/examples/barcode.data.json') as f:
             event = json.loads(f.read())
             event['eventName'] = '/billing'
-            return call_node_lambda(command=["node", "dist/app.js"], event=event)
+            return call(command=["node", "dist/app.js"], event=event)
 
     port = int(os.environ['PORT']) if os.environ.get('PORT') is not None else None
     app.run(debug=True, port=port)
--- src/app.tsx
+++ src/app.tsx
@@ -13,26 +13,57 @@ import { barcodeTestHandler }    from './examples/barcode-test';
 // tslint:disable-next-line:no-eval
 const requireDynamic = eval("require");
 
-const data = kanbanData;
-const handler = barcodeTestHandler;
-
-// handler(data, {} as any, (error, result) => {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         const fs = require('fs');
-//         if (!fs.existsSync('./debug')) {
-//             fs.mkdirSync('./debug');
-//         }
-//         fs.writeFileSync('./debug/index.html', result);
-//     }
-// });
+let handler = barcodeTestHandler;
+let data: any = kanbanData;
 
 
-App
-.route('/', (evt, ctx, cb) => cb(null, 'Hello, Node!'))
-.route('/billing', billngReportHandler)
-.route('/kanban', kanbanReportHandler)
-.route('/fba-a4', fbaA4ReportHandler)
-.route('/barcode-test', barcodeTestHandler)
-.run({});
+let debug = false;
+
+for (const arg of process.argv) {
+    if (arg === '--debug') {
+        debug = true;
+    } else if (arg.startsWith('--handler=')) {
+        switch (arg.replace('--handler=', '')) {
+        case '/billing':
+            handler = billngReportHandler;
+            data = billngData;
+            break;
+        case '/kanban':
+            handler = kanbanReportHandler;
+            data = kanbanData;
+            break;
+        case '/fba-a4':
+            handler = fbaA4ReportHandler;
+            data = kanbanData;
+            break;
+        case '/barcode-test':
+            handler = barcodeTestHandler;
+            data = kanbanData;
+            break;
+        }
+    }
+}
+
+
+if (debug) {
+    handler(data, {} as any, (error, result) => {
+        if (error) {
+            console.log(error);
+        } else {
+            const fs = require('fs');
+            if (!fs.existsSync('./debug')) {
+                fs.mkdirSync('./debug');
+            }
+            fs.writeFileSync('./debug/index.html', result);
+        }
+    });
+} else {
+    App
+    .route('/', (evt, ctx, cb) => cb(null, 'Hello, Node!'))
+    .route('/billing', billngReportHandler)
+    .route('/kanban', kanbanReportHandler)
+    .route('/fba-a4', fbaA4ReportHandler)
+    .route('/barcode-test', barcodeTestHandler)
+    .run({});
+}
+
